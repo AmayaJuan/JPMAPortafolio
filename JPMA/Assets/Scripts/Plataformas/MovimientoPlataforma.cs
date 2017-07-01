a3ff248a31e77edaeb983e65a3c2121e7d994461
AmayaJuan <jpva360@gmail.com>
2017-07-01 11:01:51 -0500

    bbb
    nnnb

--- JPMA/Assets/Scripts/Plataformas/MovimientoPlataforma.cs
+++ JPMA/Assets/Scripts/Plataformas/MovimientoPlataforma.cs
@@ -6,17 +6,17 @@ public class MovimientoPlataforma : MonoBehaviour
 {
     public bool moverHorizontal = true;
 
-    public int mov;
-
-    public float speed;
-
+    int mov = 5;
+    float speed = 2f;
     float posInicialX;
     float posInicialY;
+    Player player;
 
     void Start ()
     {
         posInicialX = transform.position.x;
         posInicialY = transform.position.y;
+        player = FindObjectOfType<Player>();
 	}
 	
 	void Update ()
@@ -25,5 +25,17 @@ public class MovimientoPlataforma : MonoBehaviour
             transform.position = new Vector3(Mathf.PingPong(Time.time * speed, mov) + posInicialX, transform.position.y, 0);
         else
             transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * speed, mov) + posInicialY, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Walu_Player")
+            player.transform.parent = transform;
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Walu_Player")
+            player.transform.parent = null;
     }
 }
--- JPMA/Assets/Scripts/Player/Player.cs
+++ JPMA/Assets/Scripts/Player/Player.cs
@@ -2,9 +2,15 @@
 
 public class Player : MonoBehaviour
 {
+    public bool enSuelo;
+    public Transform pie;
+    public LayerMask suelo;
+    public float fuerzaSalto = 100f;
+    public float radioPie;
+
     float speed = 4f;
     float movement = 0;
-
+  
     Rigidbody2D rb;
     Animator animator;
     SpriteRenderer sr;
@@ -23,17 +29,32 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+       
+    }
+
+    void FixedUpdate()
+    {
         movement = Input.GetAxisRaw("Horizontal") * speed;
         animator.SetInteger("VelX", Mathf.RoundToInt(movement));
 
         if (movement < 0)
             sr.flipX = true;
-        else
+        else if(movement > 0)
             sr.flipX = false;
-    }
 
-    void FixedUpdate()
-    {
         rb.MovePosition(rb.position + Vector2.right * movement * Time.fixedDeltaTime);
+      
+        enSuelo = Physics2D.OverlapCircle(pie.position, radioPie, suelo);
+        if (enSuelo)
+        {
+            animator.SetBool("Jump", false);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(new Vector2(0, fuerzaSalto));
+                animator.SetBool("Jump", true);
+            }
+        }
+        else
+            animator.SetBool("Jump", false);    
     }
 }
